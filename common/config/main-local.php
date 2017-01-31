6d493fd2ebfa7669e0a97b5e215cbccd75c5b312
cophiel <cophiel@gmail.com>
2017-02-01 00:47:48 +0800

    Add files via upload

---
+++ common/config/main-local.php
@@ -0,0 +1,20 @@
+<?php
+return [
+    'components' => [
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=localhost;dbname=a0117174039',
+            'username' => 'a0117174039',
+            'password' => '6d1fec28',
+            'charset' => 'utf8',
+        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'viewPath' => '@common/mail',
+            // send all mails to a file by default. You have to set
+            // 'useFileTransport' to false and configure a transport
+            // for the mailer to send real emails.
+            'useFileTransport' => true,
+        ],
+    ],
+];
